0d7802db638f59fde474a2baa77207210b3d2ac6
Max Starr <max.starr@techie.com>
2017-03-21 10:27:40 -0700

    Maybe allow external domains to connect?

--- app.js
+++ app.js
@@ -56,6 +56,19 @@ router.get('/:school', function (req, res) {
     }
 });
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+     // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+});
+
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/', router);
