6f238a26b67b54fbe5be7039a87cd8cc2ed31715
Max Starr <max.starr@techie.com>
2017-03-21 10:03:57 -0700

    Added debug and cleaned up request protocol

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ let http = require('http');
 // require config files and data
 let schools = require('./schools.json');
 
+// initiate the date object
+let date = new Date();
+let dayi = date.getDay();
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({
@@ -28,24 +32,20 @@ var router = express.Router(); // get an instance of the express Router
 // middleware to use for all requests
 router.use(function (req, res, next) {
     // do logging
-    console.log('Something is happening.');
+    console.log('Recived a ' + req.method + ' request from ' + req.ip + ' on ' + req.hostname + ' to ' + req.originalUrl);
     next(); // make sure we go to the next routes and don't stop here
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/:school/:dayi', function (req, res) {
-    if (req.params.dayi < 0 || req.params.dayi > 6) {
-        res.json({
-            error: 'Invalid day'
-        });
-    } else if(!schools.includes(req.params.school)){
+router.get('/:school', function (req, res) {
+    if(!schools.includes(req.params.school)){
         res.json({
             error: "Unknown school",
             school: req.params.school
         })
     } else {
         let schedule = require('./' + req.params.school);
-        let day = schedule[req.params.dayi-1];
+        let day = schedule[dayi-1];
         res.json({
             data: day
         });
